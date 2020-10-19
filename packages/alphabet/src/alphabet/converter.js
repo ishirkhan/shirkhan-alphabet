22dc968b86ca5caeeea85299294477e41a160b75
babbage <601728820@qq.com>
2020-10-19 10:38:50 +0800

    feat: 新增了一个单词转换器占位,用来给alphabet提供统一转换规则

---
+++ packages/alphabet/src/alphabet/converter.js
@@ -0,0 +1,12 @@
+export const DIRECTION_SHIRKHAN2UIGHUR = "shirkhan2uighur";
+export const DIRECTION_UIGHUR2SHIRKHAN = "uighur2shirkhan";
+
+/**
+ * 把单词按照要求互换成目标内容
+ * @param {string} word
+ * @param {"shirkhan2uighur"|"uighur2shirkhan"} direction shirkhan2uighur
+ * @returns {string} 转换后的结果
+ */
+export function WordConverter(word, direction) {
+  return "";
+}
