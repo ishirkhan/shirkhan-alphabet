f6ab4bb10056456ddd0418ef5f3119a7e3041208
babbage <601728820@qq.com>
2021-11-04 15:10:44 +0800

    feat: reame update,added todo,developer guide

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const { defineConfig } = require("vite");
+
+module.exports = defineConfig({
+  build: {
+    minify: true,
+    outDir: "dist",
+    sourcemap: false,
+    lib: {
+      name: "shirkhan-alphabet",
+      entry: path.resolve(__dirname, "src/index.ts"),
+      fileName: (format) => `shirkhan-alphabet.${format}.js`,
+    },
+    rollupOptions: {},
+  },
+});
