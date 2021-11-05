6bdc0d3afd793e525d9645d290c5b26d161b187c
babbage <601728820@qq.com>
2021-11-05 16:38:16 +0800

    feat: 新增Todo,代码精华

--- src/alphabet/converter/base.ts
+++ src/alphabet/converter/base.ts
@@ -7,31 +7,31 @@ export class Base implements Contract {
   alphabet = new Alphabet();
 
   orderedTable(): IAlphaItem[] {
-    return this.alphabet.getTable().sort((a: any, b: any) => {
-      return b[this.type]?.length - a[this.type]?.length;
-    });
+    return this.alphabet
+      .getTable()
+      .sort((a: any, b: any) => b[this.type]?.length - a[this.type]?.length);
   }
 
   getMap(): { [k: string]: string } {
     const kvmap: { [k: string]: string } = {};
-    this.orderedTable().forEach((item) => {
-      kvmap[item[this.type]] = item.uchar;
-    });
+    this.orderedTable().forEach(
+      (item) => (kvmap[item[this.type]] = item.uchar)
+    );
     return kvmap;
   }
 
   convert(uword: string): string {
-    Object.entries(this.getMap()).forEach(([key, value]) => {
-      uword = uword.replaceAll(value, key);
-    });
+    Object.entries(this.getMap()).forEach(
+      ([key, value]) => (uword = uword.replaceAll(value, key))
+    );
 
     return uword;
   }
 
   forward(word: string): string {
-    Object.entries(this.getMap()).forEach(([key, value]) => {
-      word = word.replaceAll(key, value);
-    });
+    Object.entries(this.getMap()).forEach(
+      ([key, value]) => (word = word.replaceAll(key, value))
+    );
     return word;
   }
 }
